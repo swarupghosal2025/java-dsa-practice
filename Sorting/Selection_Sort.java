11f77f2f28560e2b4dbef64fda1f5fa6907e3b8f
Swarup Ghosal <ghosalswarup18@gmail.com>
2026-09-20 11:46:47 +0530

    Add Selection Sort implementation

---
+++ Sorting/Selection_Sort.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class Selection_Sort{
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 8, 1, 2};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void selectionSort(int[] arr){
+        for(int i = 0; i< arr.length; i ++){
+            int last = arr.length - i - 1;
+            int maxElement = getMaxElement(arr, 0, last);
+            swap(arr, maxElement, last);
+        }
+    }
+
+    static void swap(int[] arr,int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    static int getMaxElement(int[] arr, int start, int end){
+        int max = start;
+        for(int i = start+1; i <= end; i++){
+            if(arr[max] < arr[i])
+                max = i;
+        }
+        return max;
+    }
+}
